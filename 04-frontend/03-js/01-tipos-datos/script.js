5c6cd5b6654a82a02e4406425e766da3f65775d5
angelo <angelogabrielmolinacampannell@gmail.com>
2023-06-29 17:34:56 -0500

    esto es tipos de datos y condicionales

---
+++ 04-frontend/03-js/01-tipos-datos/script.js
@@ -0,0 +1,23 @@
+var nombre = "angelo";
+//console log para mostrar algo en consola
+console.log(nombre);
+
+//tipos de datos
+37; //number
+true; //boolean
+("hola mundo"); //string
+undefined;
+null;
+
+//tipos de datos objetos
+
+[37, true, false, "hola mundo"]; //array
+
+//variables
+var nombre = "angelo";
+var edad = 17;
+
+//console log para mostrar algo en la consola del navegador
+console.log("hola mi nombre es:" + nombre + "y tengo " + edad);
+
+alert("hola sakura");
